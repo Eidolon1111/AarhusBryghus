1633663e9b75fec58335f7fd2fd352d22fea3c8b
sebastianhaug <sebastian.haug@live.dk>
2022-10-26 12:04:45 +0200

    Merge og commit

--- src/Gui/OpretPrisPane.java
+++ src/Gui/OpretPrisPane.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import org.w3c.dom.Text;
+
 
 public class OpretPrisPane extends GridPane {
 
