23b8afbaf548c607c150670924d0baa4f6e22d29
sebastianhaug <sebastian.haug@live.dk>
2022-11-02 08:08:27 +0100

    Debugging af Gui og implementation af AfregnUdlejningPane gui

--- src/Gui/DagsrapporterPane.java
+++ src/Gui/DagsrapporterPane.java
@@ -2,14 +2,13 @@ package Gui;
 
 import Application.Model.SimpeltSalg;
 import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
 
 public class DagsrapporterPane extends GridPane {
 
     private ControllerInterface controller;
-
-    private ListView<SimpeltSalg> lWSalg = new ListView<>();
 
     public DagsrapporterPane(ControllerInterface controller){
         this.controller = controller;
@@ -20,7 +19,6 @@ public class DagsrapporterPane extends GridPane {
         this.setPrefWidth(1000);
         this.setPrefHeight(800);
 
-        //this.add(lWSalg, 0, 0);
     }
 
     public void updateControls(){
