f26f742562688f21b7247bd60362c357c78328d3
sebastianhaug <sebastian.haug@live.dk>
2022-11-10 10:29:58 +0100

    treeview implementeret!

--- src/Gui/OpretPrisPane.java
+++ src/Gui/OpretPrisPane.java
@@ -3,17 +3,22 @@ package Gui;
 import Application.Model.Pris;
 import Application.Model.Prisliste;
 import Application.Model.Produkt;
+import Application.Model.ProduktGruppe;
 import javafx.beans.value.ChangeListener;
 import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+
+import java.util.ArrayList;
 
 
 public class OpretPrisPane extends GridPane {
     
     private ControllerInterface controller;
     private ListView<Prisliste> lwPrislister = new ListView<>();
-    private ListView<Produkt> lwProdukter = new ListView<>();
+    //private ListView<Produkt> lwProdukter = new ListView<>();
+    private TreeView<Object> treeViewProdukterIproduktgrupper = new TreeView<>();
+    private TreeItem<Object> root = new TreeItem<>("ProduktGrupper");
     private ListView lwValgtPrisliste = new ListView<>();
     private TextField txfOpretPrisliste = new TextField();
     private TextField txfDkkPris, txfKlipPris;
@@ -29,14 +34,11 @@ public class OpretPrisPane extends GridPane {
         this.setVgap(10);
         this.setGridLinesVisible(false);
 
-        //Oprettelse af elementer i række 1
         Label lblPrislister = new Label("Vælg prisliste: ");
         Label lblEllerOpretNy = new Label("Eller opret ny prisliste: ");
         Label lblOpretPrisListe = new Label("Prisliste navn: ");
         Button btnOpret = new Button("Opret");
 
-
-        //Tilføjelse af elementer i række 1
         this.add(lblPrislister, 0, 0);
         this.add(lwPrislister, 0, 1,2,16);
         this.add(lblEllerOpretNy, 0, 18);
@@ -47,15 +49,12 @@ public class OpretPrisPane extends GridPane {
         this.add(lblErrorPrisliste, 1, 21);
         lblErrorPrisliste.setStyle("-fx-text-fill: red");
 
-
-        //Oprettelse af elementer i række 2
         Label lblProdukter = new Label("Produkter:");
 
-        //Tilføjelse af elementer i række 2
         this.add(lblProdukter, 2, 0);
-        this.add(lwProdukter, 2, 1,2,16);
+        //this.add(lwProdukter, 2, 1,2,16);
+        this.add(treeViewProdukterIproduktgrupper, 2, 1,2,16);
 
-        //Oprettelse af elementer i række 3
         Button btnTilføj = new Button("Tilføj/Gem");
 
         Label lblDkkPris = new Label("DKK pris: ");
@@ -63,9 +62,6 @@ public class OpretPrisPane extends GridPane {
 
         Label lblKlipPris = new Label("Klip pris: ");
         txfKlipPris = new TextField();
-
-
-        //Tilføjelse af elementer i række 3
 
         this.add(lblDkkPris, 4, 5);
         this.add(txfDkkPris, 5, 5);
@@ -76,27 +72,27 @@ public class OpretPrisPane extends GridPane {
         this.add(lblErrorPris, 5, 8);
         lblErrorPris.setStyle("-fx-text-fill: red");
 
-        //Oprettelse af elementer i række 4
         Label lblValgtPrisliste = new Label("Valgt prisliste: ");
         Button btnFjern = new Button("Fjern");
 
-        //Tilføjelse af elementer i række 4
+
         this.add(lblValgtPrisliste, 6, 0);
         this.add(lwValgtPrisliste, 6, 1,2,16);
         this.add(btnFjern, 6, 17);
-        
-        //Vinduestørrelse preset
+
         this.setPrefHeight(400);
         this.setPrefWidth(1000);
-        
-        //Indsat data i listviews
+
         lwPrislister.getItems().setAll(controller.getPrislister());
         ChangeListener<Prisliste> prislisteListener = (ov, o, n) -> PrislisteItemSelected();
         lwPrislister.getSelectionModel().selectedItemProperty().addListener(prislisteListener);
 
-        lwProdukter.getItems().setAll(controller.getProdukter());
+        //lwProdukter.getItems().setAll(controller.getProdukter());
+        treeViewProdukterIproduktgrupper.setRoot(root);
+        treeViewProdukterIproduktgrupper.setShowRoot(false);
+        ChangeListener<TreeItem<Object>> objectChangeListener = (ov, o, n) -> treeviewItemSelectedChanged();
+        treeViewProdukterIproduktgrupper.getSelectionModel().selectedItemProperty().addListener(objectChangeListener);
 
-        //Buttons funktionalitet
         btnOpret.setOnAction(event -> this.opretAction());
         btnTilføj.setOnAction(event -> this.tilføjAction());
         btnFjern.setOnAction(event -> this.fjernAction());
@@ -121,24 +117,30 @@ public class OpretPrisPane extends GridPane {
 
     public void tilføjAction() {
         Prisliste pl = lwPrislister.getSelectionModel().getSelectedItem();
-        Produkt p = lwProdukter.getSelectionModel().getSelectedItem();
-        if (p != null && pl != null) {
-            if (txfDkkPris.getText().isEmpty()) {
-                lblErrorPris.setText("Pris mangler");
-            } else if (txfKlipPris.getText().isEmpty() && Integer.parseInt(txfDkkPris.getText()) >= 0) {
-                double pris = Integer.parseInt(txfDkkPris.getText());
-                controller.createPris(pl, p, pris, 0);
-                updateControls();
-            } else if (Integer.parseInt(txfDkkPris.getText()) <= 0){
-                lblErrorPris.setText("Pris skal være et positivt tal");
-            }else if (Integer.parseInt(txfDkkPris.getText()) >= 0 && Integer.parseInt(txfKlipPris.getText()) >= 0){
-                double pris = Integer.parseInt(txfDkkPris.getText());
-                int klip = Integer.parseInt(txfKlipPris.getText());
-                controller.createPris(pl,p,pris, klip);
-                updateControls();
+        TreeItem<Object> treeItem = treeViewProdukterIproduktgrupper.getSelectionModel().getSelectedItem();
+        Produkt p;
+        if(treeItem.getValue() instanceof Produkt){
+            p = (Produkt) treeItem.getValue();
+            if (p != null && pl != null) {
+                if (txfDkkPris.getText().isEmpty()) {
+                    lblErrorPris.setText("Pris mangler");
+                } else if (txfKlipPris.getText().isEmpty() && Integer.parseInt(txfDkkPris.getText()) >= 0) {
+                    double pris = Integer.parseInt(txfDkkPris.getText());
+                    controller.createPris(pl, p, pris, 0);
+                    updateControls();
+                } else if (Integer.parseInt(txfDkkPris.getText()) <= 0){
+                    lblErrorPris.setText("Pris skal være et positivt tal");
+                }else if (Integer.parseInt(txfDkkPris.getText()) >= 0 && Integer.parseInt(txfKlipPris.getText()) >= 0){
+                    double pris = Integer.parseInt(txfDkkPris.getText());
+                    int klip = Integer.parseInt(txfKlipPris.getText());
+                    controller.createPris(pl,p,pris, klip);
+                    updateControls();
+                }
+            } else {
+                lblErrorPris.setText("Der mangler at blive valgt prisliste og/eller produkt");
             }
         } else {
-            lblErrorPris.setText("Der mangler at blive valgt prisliste og/eller produkt");
+            lblErrorPris.setText("Du skal vælge et produkt");
         }
     }
 
@@ -152,7 +154,9 @@ public class OpretPrisPane extends GridPane {
     }
 
     public void updateControls(){
-        lwProdukter.getItems().setAll(controller.getProdukter());
+        root.getChildren().clear();
+        createProduktGruppeBranches(controller.getProduktGrupper(), root);
+        //lwProdukter.getItems().setAll(controller.getProdukter());
         Prisliste pl = lwPrislister.getSelectionModel().getSelectedItem();
         if (pl != null) {
             lwValgtPrisliste.getItems().setAll(pl.getPrislisten());
@@ -166,6 +170,26 @@ public class OpretPrisPane extends GridPane {
 
     public void PrislisteItemSelected() {
         this.updateControls();
+    }
+
+    public void treeviewItemSelectedChanged(){
+        TreeItem<Object> treeItem = treeViewProdukterIproduktgrupper.getSelectionModel().getSelectedItem();
+    }
+
+    public void createProduktGruppeBranches(ArrayList<ProduktGruppe> list, TreeItem<Object> parent){
+        for(ProduktGruppe produktGruppe : list){
+            TreeItem<Object> branchItem = new TreeItem<>(produktGruppe);
+            parent.getChildren().add(branchItem);
+            createProduktLeafs(produktGruppe.getProdukts(), branchItem);
+
+        }
+    }
+
+    public void createProduktLeafs(ArrayList<Produkt> list, TreeItem<Object> parent){
+        for(Produkt produkt : list){
+            TreeItem<Object> leafItem = new TreeItem<>(produkt);
+            parent.getChildren().add(leafItem);
+        }
     }
 
 }
